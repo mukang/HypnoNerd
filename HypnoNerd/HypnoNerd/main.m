73aa11958b8edadbc1c70e67e6189fc81c90104b
mukang <mukang.max@foxmail.com>
2016-10-11 16:14:34 +0800

    新建项目HypnoNerd

---
+++ HypnoNerd/HypnoNerd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by 穆康 on 16/10/11.
+//  Copyright © 2016年 mukang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
